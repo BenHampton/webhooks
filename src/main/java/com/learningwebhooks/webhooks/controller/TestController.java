9d2f2cccd14e3c7f5aabca4a6bc1a00210794ead
Ben <benhampton11@yahoo.com>
2021-11-29 20:21:58 -0600

    TEST MESSAGE

---
+++ src/main/java/com/learningwebhooks/webhooks/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.learningwebhooks.webhooks.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping
+    public String testController() {
+        return "Hello World";
+    }
+}
