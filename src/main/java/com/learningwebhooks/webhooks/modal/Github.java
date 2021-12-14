de8ab6283c92bee703664b7a586e9b9bcf3731a7
Ben <benhampton11@yahoo.com>
2021-12-13 22:49:43 -0600

    created to Github modal

--- src/main/java/com/learningwebhooks/webhooks/modal/Github.java
+++ src/main/java/com/learningwebhooks/webhooks/modal/Github.java
@@ -1,5 +1,6 @@
 package com.learningwebhooks.webhooks.modal;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,5 +11,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class Github {
+
     private String action;
+
+    private String ref;
+
+    private String before;
+
+    private String after;
+
+    private String compare;
+
+//    private Repository repository;
+//
+//    private Pusher pusher;
+//
+//    private Sender sender;
+//
+//    private Commits commits;
+//
+    @JsonProperty(value = "head_commit")
+    private Commits headCommit;
 }
